99c722f6b1f697a9fbb3797f7ecc8de5cf7213fe
wzx <541036103@qq.com>
2022-08-07 21:49:48 +0800

    build

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,9 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/dist/',
+  base: '/docs/',
+  build: {
+    outDir: 'docs',
+  },
   plugins: [vue()]
 })
